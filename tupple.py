698874a6cb5904c80cde31159cfd491194fffe79
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-15 20:57:53 +0530

    tupple

---
+++ tupple.py
@@ -0,0 +1,24 @@
+#tuple
+# it is a container
+# it is a sequence datatype (order datatype)
+# it is a immutable data type.
+# (a)representation()
+# b) comma seperated
+# it is faster than list
+# single element representation
+ # it is stored in the form of tuple or dictionary
+lis=[2,33,2]
+lis[1]=36
+print(lis)
+
+fruit = ("apple",20,100)
+print(type(fruit))
+#fruit[2]=200 #tuple immutable
+print(id(fruit))
+print(id(fruit[0])),print(id(fruit[0])),print(id(fruit[2]))
+
+fruit = ("apple",20,100)
+fruit = ("apple",20,100)+("banana",999)
+fruit = 999999999999
+print(fruit)
+# fruit
