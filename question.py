1e6e9ac28d3605156a3f16aba71c3414481a2554
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-17 08:17:42 +0530

    question tuple

---
+++ question.py
@@ -0,0 +1,71 @@
+data = [10,[2.5,"apple"],(3,4)]
+print(data)
+a=data[1][1]
+print(a)
+
+items = [(1,2,3),["banana","kiwi",7.5]]
+print(items)
+b=items[0][2]
+print(b)
+
+mixed = [[1,2],(3.5,"grape"),[4,5,6]]
+print(mixed)
+a=mixed[1][0]
+print(a)
+
+elements = [100,(200,300),["a","b","c"]]
+print(elements)
+a=elements[2][1]
+print(a)
+
+data1 = [[10,20],(30,40,50)]
+print(data1)
+a=data1[0][0]
+print(a)
+
+values = [(1.5,2.5),[3,4],5]
+print(values)
+a=values[0][1]
+print(a)
+
+numbers = [[5,10],(15,20)]
+print(numbers)
+a=numbers[1][1]
+print(a)
+
+shapes = ["circle",(5,10),[15,20,25]]
+print(shapes)
+a=shapes[2][0]
+print(a)
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix)
+a=matrix[1][2]
+print(a)
+
+data2 = [(1,2,3),[4,5,6]]
+print(data2)
+data2[0][1]=10
+print(data2)
+
+data3 = [[1,2],(3.5,4.5)]
+print(data3)
+data3[1][0]=7.5
+print(data3)
+
+
+
+
+
+
+# question 1 ----> answer = apple
+# question 2 ----> answer = 3
+# question 3 ----> answer = 3.5
+# question 4 ----> answer = "b"
+# question 5 ----> answer = 10
+# question 6 ----> answer = 2.5
+# question 7 ----> answer = 20
+# question 8 ----> answer = 15
+# question 9 ----> answer = 6
+# question 10 ----> answer = error
+# question 11 ----> answer = error
