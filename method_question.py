721bfb427c329809ac3e6c5adf5124b3828ffdde
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-23 20:45:05 +0530

    set

--- method_question.py
+++ method_question.py
@@ -1,4 +1,8 @@
 data = {'1':2390,'2':2387,'3':4533,'4':{'11':3523,'22':63634,'44':4574}}
-print(data['4'])
-print(data['4'][4574])
+print(type(data['4']))
+print(type(data['1']))
+print(type(data))
+data['4'].popitem()
+print(data)
+data.popitem()
 print(data)
---
+++ set.py
@@ -0,0 +1,16 @@
+# set:
+# set is a container in which we contain multiple data, but 
+# duplication is not allowed, and unordered datatype
+# where our main concern is data, but not any order related to it,
+# then we use set.
+# collection of alphabet ---> super set
+# a,e,i,o,u ----> subset
+
+# A = {1,3,9,9,7,2,5}
+# B = {2,3,99,11,99,5}
+# A intersection B ---> {3,2,5}
+# A union B ---> {1,7,9,3,2,5,99,11}
+# A-B ---> {1,7,9}
+# B-A ---> {99,11}
+# intersection means common 
+# union means both set
