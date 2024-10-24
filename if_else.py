141dec2323d5af755986fbbbbec45b469543c11b
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-24 17:50:59 +0530

    if_else

---
+++ if_else.py
@@ -0,0 +1,16 @@
+'''
+condition statement (decision making)
+control flow statement(looping)
+Diffrence if /else
+I work in single condition
+if/ else ladder
+more than one condition   example=grade,electricity,bill
+if/else nested
+level wise example=>password,entrance exam
+'''
+age = int(input("enter age:"))
+if age>=18:
+    print("eligible")
+else:
+    print("not eligible")
+
--- set.py
+++ set.py
@@ -14,3 +14,33 @@
 # B-A ---> {99,11}
 # intersection means common 
 # union means both set
+s1 = {1,2,3,3,4,9,11,17}
+s2 = {4,5,9,17,89}
+s3 = {"a","b",1,2,3,88}
+print(s1)
+r=s1.union(s2)
+print(r)
+r=s1.intersection(s2)
+print(r)
+r=s1.union(s2,s3)
+print(r)
+r=s1.intersection(s2,s3)
+print(r)
+r = s1.difference(s2)
+print(r)
+# update in s1
+s1.update(s2)
+print(s1)
+# add take all object
+s1.add(99)
+print(s1)
+# add tuple
+s1.add((5,))
+print(s1)
+
+# r = s1|s2|s3 ---> union
+# r = s1&s2&s3 ---> intersection
+# r = s1-s2-s3 ---> difference means ignore common and s2
+
+
+
